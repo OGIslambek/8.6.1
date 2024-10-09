5db85728cff17820da7ea45f494a11a732b1043e
OGislambek <ogislambek@gmail.com>
2024-10-09 23:58:57 +0300

    .

--- 8.6.1/Program.cs
+++ 8.6.1/Program.cs
@@ -1,11 +1,17 @@
 ﻿using System.IO;
+using static _8._6._1.FileName;
 
 namespace _8._6._1;
 
 internal class Program
 {
+    static string path;
     static void Main(string[] args)
     {
-        
+        Console.WriteLine("Введите путь до папки");
+
+        path = Console.ReadLine();
+
+        CleanDirectory.Cleaner(path);
     }
 }
